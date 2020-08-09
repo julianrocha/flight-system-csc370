fe3e260612f9a7608e65030ab2bccfa32e92c8c8
Julian Rocha <jules.r.rocha@gmail.com>
2020-08-09 00:33:11 -0700

    added trigger functions and test cases

--- solution/create_schema.sql
+++ solution/create_schema.sql
@@ -1,6 +1,8 @@
 -- Julian Rocha V00870460
 
--- If the tables/functions already exist, delete them --
+rollback;
+
+-- If the tables/functions already exist, delete them
 drop table if exists reservations;
 drop table if exists passengers;
 drop table if exists flights;
@@ -9,6 +11,10 @@ drop table if exists airports;
 
 drop function if exists aircraft_capacity_trigger();
 drop function if exists international_flight_trigger();
+drop function if exists passengers_ignore_duplicates_trigger();
+drop function if exists aircraft_location_trigger();
+drop function if exists aircraft_timing_trigger();
+
 
 -- Create airports table --
 create table airports( 
@@ -37,19 +43,16 @@ create table flights(
 	aircraft_id varchar(64),
 	foreign key (src) references airports(airport_code)
 		on delete restrict
-		on update restrict,
+		on update cascade,
 	foreign key (dst) references airports(airport_code)
 		on delete restrict
-		on update restrict,
+		on update cascade,
 	foreign key (aircraft_id) references aircrafts(aircraft_id)
 		on delete restrict
-		on update restrict,
+		on update cascade,
 	constraint different_airports check(src != dst),
 	constraint chronological_order check(departure < arrival)
 	-- check that flight airline matches aircraft airline
-	-- check each aircraft must be on the ground for at least 60 minutes between flights
-	-- check the same aircraft can’t be used for two flights at the same time
-	-- check the aircraft must take off from the same airport where it landed
 	);
 
 -- Create passengers table --
@@ -131,12 +134,72 @@ create trigger airports_international_flight_trigger
 	after update on airports
 	execute procedure international_flight_trigger();
 
+-- silently ignore duplicate entries of passengers
+create function passengers_ignore_duplicates_trigger()
+returns trigger as
+$BODY$
+begin
+if (select count(*) from passengers where passenger_id = new.passenger_id and passenger_name = new.passenger_name) > 0 then
+	return null;
+elsif (select count(*) from passengers where passenger_id = new.passenger_id) > 0 then
+	raise exception 'Cannot change name of existing passenger'; 
+end if;
+return new;
+end
+$BODY$
+language plpgsql;
+
+create trigger passengers_ignore_duplicates_trigger
+	before insert on passengers
+	for each row
+	execute procedure passengers_ignore_duplicates_trigger();
+
+-- check the aircraft must take off from the same airport where it landed
+create function aircraft_location_trigger()
+returns trigger as
+$BODY$
+begin
+if (with
+		prev_dst as(
+			select *, max(dst) over(partition by aircraft_id order by departure rows between 1 preceding and 1 preceding) as prev_dst from flights
+		)
+	select count(*) from prev_dst where src != prev_dst
+	) > 0 then
+	raise exception 'Aircraft must takeoff from same airport where it landed'; 
+end if;
+return new;
+end
+$BODY$
+language plpgsql;
+
+create constraint trigger flights_aircraft_location_trigger
+	after insert or update or delete on flights
+	deferrable
+	for each row
+	execute procedure aircraft_location_trigger();
 
 
-select * from airports;
-select * from aircrafts;
-select * from flights;
-select * from passengers;
-select * from reservations;
+-- check each aircraft must be on the ground for at least 60 minutes between flights
+-- check the same aircraft can’t be used for two flights at the same time
+create function aircraft_timing_trigger()
+returns trigger as
+$BODY$
+begin
+if (with
+		ground_time as(
+			select *, (extract(epoch from departure) - extract( epoch from max(arrival) over(partition by aircraft_id order by departure rows between 1 preceding and 1 preceding)))/60 as ground_mins from flights
+		)
+	select count(*) from ground_time where ground_mins < 60
+	) > 0 then
+	raise exception 'Each aircraft can only be used by one flight at a time and must be grounded for at least 60 minutes'; 
+end if;
+return new;
+end
+$BODY$
+language plpgsql;
+
+create trigger aircraft_timing_trigger
+	after insert or update on flights
+	execute procedure aircraft_timing_trigger();
 
 
